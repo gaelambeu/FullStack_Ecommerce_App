57b80f1a21998996f5aaef3f8470c2a4043da8ae
Gael16062003 <ambeugael128@gmail.com>
2023-05-05 13:38:56 +0200

    end/begin
    end to create Register
    begin to create AuthContext

--- front-end/src/screens/RegisterScreen.js
+++ front-end/src/screens/RegisterScreen.js
@@ -1,12 +1,77 @@
-import React from "react";
-import { Text, View } from "react-native"
+import React, {useState} from "react";
+import { 
+    Text, 
+    TextInput, 
+    View, 
+    Button, 
+    TouchableOpacity, 
+    StyleSheet,
+} from "react-native"
 
-const RegisterScreen = () => {
+const RegisterScreen = ({navigation}) => {
+
+    const [name, setName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    
+
     return (
-        <View>
-            <Text> RegisterScreen </Text>
+        <View style={styles.container}>
+            <View style={styles.wrapper}>
+                <TextInput 
+                    style={styles.input} 
+                    value={name} 
+                    placeholder="Enter name" 
+                    onChangeText={text => setName(text)}
+                />  
+                
+                <TextInput 
+                    style={styles.input} 
+                    value={email} 
+                    placeholder="Enter email" 
+                    onChangeText={text => setEmail(text)}
+                />
+
+                <TextInput 
+                    style={styles.input} 
+                    value={password} 
+                    placeholder="Enter password"
+                    onChangeText={text => setPassword(text)} 
+                    secureTextEntry 
+                />
+
+                <Button title="Register" />
+
+                <View style={{flexDirection:'row', marginTop: 20}}>
+                    <Text>Already have an account?</Text>
+                    <TouchableOpacity onPress={() => navigation.navigate('Login')} >
+                        <Text style={styles.link} >Login</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    wrapper:{
+        width: '80%',
+    },
+    input: {
+        marginBottom: 12,
+        borderWidth: 1,
+        borderColor: '#bbb',
+        borderRadius: 5,
+        paddingHorizontal: 14,
+    },
+    link: {
+        color: 'blue',
+    },
+});
 
 export default RegisterScreen;
