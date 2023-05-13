31b5aff6ee08b2b66e5167be96e771b755e7086d
Gael16062003 <ambeugael128@gmail.com>
2023-05-13 14:17:26 +0200

    créer le Get

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,15 +1,13 @@
 const express = require("express")
 const routes = express.Router()
-const { addNewContact } = require('../controllers/crmControllers')
+const { addNewContact, getContacts } = require('../controllers/crmControllers')
 
 routes.get('/contact', (req, res, next) => {
     // middleware
     console.log(`request of : ${req.originalUrl}`);
     console.log(`request of : ${req.method}`);
     next();
-},(req, res, next) =>{
-    res.send("ask GET with success")}
-)
+}, getContacts)
 
 routes.post(addNewContact)
 
