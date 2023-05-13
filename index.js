c9d81592f64fedfebbfc2f67c77a9bb70b1804bf
Gael16062003 <ambeugael128@gmail.com>
2023-05-13 12:58:33 +0200

    config db

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 const routes = require("./src/routes/crmRoutes");
 
 
 const app = express();
-const PORT = 3000
+const PORT = 3000;
+
+// connection mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CMRdb', {
+    useMongoClient: true
+});
+
+// bodyparser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 app.use(routes);
 
 app.get('/', (req, res) => {
