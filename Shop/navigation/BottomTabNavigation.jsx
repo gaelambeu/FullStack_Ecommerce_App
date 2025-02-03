73d160cbf02bfb72d6ecbb254c11b8466c8d8d8f
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2025-02-03 01:44:38 -0800

    write in BottomTabNavigation

--- Shop/navigation/BottomTabNavigation.jsx
+++ Shop/navigation/BottomTabNavigation.jsx
@@ -4,15 +4,43 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import Home from '../screens/Home';
 import Search from '../screens/Search';
 import Profile from '../screens/Profile';
+import { Ionicons } from '@expo/vector-icons';
 
+
+const screenOptions = {
+    tabBarShowLabel: false,
+    tabBarHideOnKeyboard: true,
+    headerShown: false,
+    tabBarStyle: {
+        position: absolute,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        elevation: 0,
+        height: 70
+    }
+}
 
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigation = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
+    <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen 
+            name="Home" 
+            component={Home} 
+            options={{
+                tabBarIcon: ({focused}) => {
+                    return <Ionicons name={focused ? "home" : "home-outline"} 
+                                size={24}
+                                color={red}
+                            />
+
+                }
+            }}
+        />
+        
       <Tab.Screen name="Search" component={Search} />
       <Tab.Screen name="Profile" component={Profile} />
 
