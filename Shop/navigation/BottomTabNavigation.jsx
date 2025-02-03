885e744ee5cfb7249cc45e0b8a2289e182d7ee36
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2025-02-03 01:12:02 -0800

    make navigation

---
+++ Shop/navigation/BottomTabNavigation.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+
+const BottomTabNavigation = () => {
+  return (
+    <View>
+      <Text>BottomTabNavigation</Text>
+    </View>
+  )
+}
+
+export default BottomTabNavigation
