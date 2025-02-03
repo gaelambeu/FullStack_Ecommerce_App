174426b5de6f618c37068961925b56bb87c25b24
GAEL AMBEU <123178233+gaelambeu@users.noreply.github.com>
2025-02-03 01:24:22 -0800

    improving

--- Shop/navigation/BottomTabNavigation.jsx
+++ Shop/navigation/BottomTabNavigation.jsx
@@ -1,13 +1,22 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import Home from '../screens/Home';
+import Search from '../screens/Search';
+import Profile from '../screens/Profile';
 
+
+
+const Tab = createBottomTabNavigator();
 
 const BottomTabNavigation = () => {
   return (
-    <View>
-      <Text>BottomTabNavigation</Text>
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Profile" component={Profile} />
+
+    </Tab.Navigator>
   )
 }
 
